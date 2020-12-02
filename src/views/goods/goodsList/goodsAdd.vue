122343481b7778e8c399d4f0dbf2288f0dda2807
LKingGG <371668749@qq.com>
2020-12-02 17:19:46 +0800

    商品添加

--- src/views/goods/goodsList/goodsAdd.vue
+++ src/views/goods/goodsList/goodsAdd.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <tinymce v-model="form.goodsDetail" :height="500" />
     <el-tabs type="border-card">
       <el-tab-pane label="基础设置">
         <el-divider content-position="left">商品类型</el-divider>
@@ -143,8 +144,8 @@
               <el-select v-model="form.region" placeholder="请选择商品规格" size="mini">
                 <el-option v-for="(item,index) in specList" :label="item.specName" :value="item.id"></el-option>
               </el-select>
-              <el-button type="primary" size="mini" @click="dialogVisible = true">快速添加规格</el-button>
-              <el-button size="mini">清空规格</el-button>
+              <el-button type="primary" size="mini" @click="fastAdd()">快速添加规格</el-button>
+              <el-button size="mini" @click="creatRow">新建规格行</el-button>
             </div>
           </div>
           <div>
@@ -158,7 +159,7 @@
                 label="规格名"
                 width="180">
                 <template slot-scope="scope">
-                  <el-input v-model="scope.row.name"></el-input>
+                  <el-input v-model="scope.row.name" @blur="specsNameChange(scope.row.name)"></el-input>
                 </template>
               </el-table-column>
               <el-table-column
@@ -169,32 +170,35 @@
                     v-for="tag in scope.row.list"
                     closable
                     :disable-transitions="false"
-                    @close="handleClose(tag)">
+                    @close="handleClose(tag,scope.row)">
                     {{tag}}
                   </el-tag>
                   <el-input
                     class="input-new-tag"
-                    v-if="inputVisible"
-                    v-model="inputValue"
+                    v-if="scope.row.inputVisible"
+                    v-model="scope.row.inputValue"
                     ref="saveTagInput"
                     size="small"
-                    @keyup.enter.native="handleInputConfirm"
-                    @blur="handleInputConfirm"
+                    @keyup.enter.native="handleInputConfirm(scope.row)"
+                    @blur="handleInputConfirm(scope.row)"
                   >
                   </el-input>
-                  <el-button v-else class="button-new-tag" size="small" @click="showInput">+ 添加规格</el-button>
+                  <el-button v-else class="button-new-tag" size="small" @click="showInput(scope.row)">+ 添加规格</el-button>
                 </template>
               </el-table-column>
               <el-table-column
                 label="操作"
-                width="180">
+                width="90">
                 <template slot-scope="scope">
-                  <el-button @click="">删除行</el-button>
+                  <el-button @click.native.prevent="deleteRow(scope.$index, specsData)" icon="" size="mini" type="warning">删除行</el-button>
                 </template>
               </el-table-column>
             </el-table>
           </div>
         </el-card>
+        <!-- <div class="mt-10">
+          <el-button type="primary" size="mini" icon="el-icon-date">生成规格sku</el-button>
+        </div> -->
         <div class="mt-10">
           批量设置：
           <el-button type="text" size="mini">销量价格</el-button>
@@ -206,39 +210,50 @@
           <el-button type="primary" size="mini">取消</el-button>
         </div>
         <el-table
-          :data="specsData"
+          :data="form.goodsSku"
           border
           style="width: 100%"
           class="mt-10"
+          v-loading="skuLoading"
           highlight-current-row>
           <el-table-column
             label="商品规格">
             <template slot-scope="scope">
-              <el-button v-for="(item,index) in scope.row.list" type="primary" size="mini">{{item}}</el-button>
+              <span v-for="(item,index) in scope.row.rulePath">{{index == 0?'':'-'}}{{item}}</span>
             </template>
           </el-table-column>
           <el-table-column
             label="销售价格"
             width="180">
-            <template><el-input size="mini"></el-input></template>
+            <template slot-scope="scope"><el-input size="mini" v-model="scope.row.salePrice"></el-input></template>
           </el-table-column>
           <el-table-column
-            prop="name"
+            prop="price"
             label="市场价格"
             width="180">
-            <template><el-input size="mini"></el-input></template>
+            <template slot-scope="scope"><el-input size="mini"></el-input></template>
           </el-table-column>
           <el-table-column
-            prop="name"
-            label="成本价格"
+            prop="skuCode"
+            label="sku编号"
             width="180">
-            <template><el-input size="mini"></el-input></template>
+            <template slot-scope="scope"><el-input size="mini"></el-input></template>
           </el-table-column>
           <el-table-column
-            prop="name"
-            label="库存"
-            width="180">
-            <template><el-input size="mini"></el-input></template>
+            label="图片"
+            width="100">
+            <template slot-scope="scope">
+              <el-upload
+                class="sku-uploader"
+                action="https://wxin.natapp4.cc/material/mt/upload/"
+                :headers="headers"
+                :show-file-list="false"
+                :on-success="(res, file)=>{return handleAvatarSuccess(res, file, scope.row)}"
+                :before-upload="beforeAvatarUpload">
+                <img v-if="scope.row.image" :src="scope.row.image" class="avatar">
+                <i v-else class="el-icon-plus sku-uploader-icon"></i>
+              </el-upload>
+            </template>
           </el-table-column>
         </el-table>
       </el-tab-pane>
@@ -276,18 +291,6 @@
       <!-- 媒体设置 start -->
       <el-tab-pane label="媒体设置">
         <el-form>
-          <el-divider content-position="left">商品图片</el-divider>
-          <el-form-item label="商品类型">
-            <el-upload
-              class="avatar-uploader"
-              action="https://jsonplaceholder.typicode.com/posts/"
-              :show-file-list="false"
-              :on-success="handleAvatarSuccess"
-              :before-upload="beforeAvatarUpload">
-              <img v-if="imageUrl" :src="imageUrl" class="avatar">
-              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-          </el-form-item>
           <el-divider content-position="left">展示视频</el-divider>
           <el-form-item label="视频上传">
             <el-upload class="avatar-uploader"
@@ -311,11 +314,24 @@
                style="margin-top:7px;"></el-progress>
             </el-upload>
           </el-form-item>
-          <el-divider content-position="left">商品详情</el-divider>
-          <el-form-item label="商品详情">
+          <el-divider content-position="left">商品图片</el-divider>
+          <el-form-item label="商品主图">
+            <el-upload
+              class="avatar-uploader"
+              action="https://wxin.natapp4.cc/material/mt/upload/"
+              :headers="headers"
+              :show-file-list="false"
+              :on-success="(res, file)=>{return mainImageUpload(res, file, scope.row)}"
+              :before-upload="beforeAvatarUpload">
+              <img v-if="form.mainImage" :src="form.mainImage" class="avatar">
+              <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="商品次图">
             <el-upload
               class="upload-demo"
-              action="https://jsonplaceholder.typicode.com/posts/"
+              action="https://wxin.natapp4.cc/material/mt/upload/"
+              :headers="headers"
               :on-preview="handlePreview"
               :on-remove="handleRemove"
               :file-list="fileList"
@@ -325,6 +341,8 @@
               <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
             </el-upload>
           </el-form-item>
+          <el-divider content-position="left">商品详情</el-divider>
+          <tinymce v-model="form.goodsDetail" :height="500" />
         </el-form>
       </el-tab-pane>
       <!-- 媒体设置 end -->
@@ -357,9 +375,12 @@
 </template>
 
 <script>
+import Tinymce from "@/components/Tinymce";
 import { getToken } from '@/utils/auth'
+import E from 'wangeditor'
 export default {
-  name: '',
+  components: { Tinymce },
+  name: 'editor',
   data() {
     return {
       form: {
@@ -379,6 +400,7 @@ export default {
         goodsDetail: '', //商品详情中显示的html文本
         categoryId: '', //商品所属分类id
         mainImage: '', //商品主图 主要用于商品列表中
+        mainKey: '', // 商品主图key，提交时需要添加到imageKeys里
         goodsVideo: '', //商品详情中商品轮播图中可以显示视频，如果存在则商品主图后挪一个位置
         images: '', //商品详情中商品轮播图 数组的json格式
         label: '', //商品标签 在商品列表/详情中显示 数组的json格式
@@ -647,13 +669,6 @@ export default {
         key:'',
         value:''
       }],
-      specsData:[{
-        name:'颜色',
-        list:['红','白']
-      },{
-        name:'这是字数特别多的时候',
-        list:['恩恩特别的多的','不拉不拉不拉不拉不拉日记']
-      }],
       videoFlag: false,
       //是否显示进度条
       videoUploadPercent: "",
@@ -673,7 +688,21 @@ export default {
         checkStrictly: true
       },
       brandList:[],
-      specList:[]
+      specList:[],
+      specsData:[{
+        name:'颜色',
+        list:['红','白'],
+        inputVisible: false,
+        inputValue: '',
+      },{
+        name:'这是字数特别多的时候',
+        list:['恩恩特别的多的','不拉不拉不拉不拉不拉日记'],
+        inputVisible: false,
+        inputValue: '',
+      }],
+      skuLoading:false, // 正在加载商品sku
+      headers: {'Authorization':getToken()},
+      editorContent: ''
     }
   },
   mounted() {
@@ -684,8 +713,16 @@ export default {
     this.$api.specList(this.params).then(res => {
       this.specList = res.data.data
     })
+    var editor = new E('#editorElem')
+    editor.customConfig.onchange = (html) => {
+      this.editorContent = html
+    }
+    editor.create()
   },
   methods: {
+    getContent() {
+      alert(this.editorContent)
+    },
     addPropertyBtn(){
       this.form.attributes.push([{'title':'','value':''}])
     },
@@ -693,19 +730,28 @@ export default {
       console.log(index, row);
       this.propertyData.splice(index,1)
     },
-    handleAvatarSuccess(res, file) {
-      this.imageUrl = URL.createObjectURL(file.raw);
+    mainImageUpload(res, file, row) { // 商品主图上传
+      console.log(res, file)
+      /* row.image = URL.createObjectURL(file.raw); */
+      this.form.mainImage = res.data.download
+      this.form.mainKey = row.data.key
+    },
+    handleAvatarSuccess(res, file, row) {
+      console.log(res, file)
+      /* row.image = URL.createObjectURL(file.raw); */
+      row.image = res.data.download
+      row.key = row.data.key
     },
     beforeAvatarUpload(file) {
       const isJPG = file.type === 'image/jpeg';
       const isLt2M = file.size / 1024 / 1024 < 2;
 
-      if (!isJPG) {
+      /* if (!isJPG) {
         this.$message.error('上传头像图片只能是 JPG 格式!');
       }
       if (!isLt2M) {
         this.$message.error('上传头像图片大小不能超过 2MB!');
-      }
+      } */
       return isJPG && isLt2M;
     },
     /* 上传视频 start */
@@ -791,25 +837,105 @@ export default {
         this.brandList = res.data
       })
     },
+    deleteRow(index, rows) { // 删除标签行
+      rows.splice(index, 1);
+    },
     /* 添加标签 */
-    handleClose(tag) {
-      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    handleClose(tag,row) {
+      row.list.splice(row.list.indexOf(tag), 1);
     },
 
-    showInput() {
-      this.inputVisible = true;
+    showInput(row) {
+      row.inputVisible = true;
       this.$nextTick(_ => {
         this.$refs.saveTagInput.$refs.input.focus();
       });
     },
 
-    handleInputConfirm() {
-      let inputValue = this.inputValue;
+    handleInputConfirm(row) {
+      let inputValue = row.inputValue;
       if (inputValue) {
-        this.dynamicTags.push(inputValue);
+        row.list.push(inputValue);
       }
-      this.inputVisible = false;
-      this.inputValue = '';
+      row.inputVisible = false;
+      row.inputValue = '';
+    },
+    testspecs(){
+      let bool = true
+      for(var i in this.specsData) {
+        if(this.specsData[i].list.length == 0 || !this.specsData[i].name){
+          bool = false
+          this.$message.warning('规格名或规格内容不能为空')
+        }
+      }
+      return bool
+    },
+    fastAdd(){ // 快速添加规格
+      let obj = {
+        name:'',
+        list:[],
+        inputVisible: false,
+        inputValue: '',
+      }
+      this.specsData.push(obj)
+    },
+    creatRow(){ // 快速添加规格
+      let obj = {
+        name:'',
+        list:[],
+        inputVisible: false,
+        inputValue: '',
+      }
+      this.specsData.push(obj)
+    },
+    specsNameChange(name) { // 修改规格名
+      if(!name){
+        this.$message.warning('规格名为空，无法更新商品sku')
+      }else{
+        this.loadGoodsSku()
+      }
+    },
+    loadGoodsSku(){
+      this.skuLoading = true
+      var arr = this.getCombination(this.specsData)
+      this.form.goodsSku = []
+      for(var i in arr){
+        let obj = {
+          price:'', //普通商品显示价格
+          salePrice:'', //普通商品显示价格
+          stock:'', //普通商品库
+          skuCode:'', //sku编号
+          rulePath: arr[i], //规格 16G-暗夜白
+          image:'', //完整规格的商品主图
+          key:'' //图片key
+        }
+        this.form.goodsSku.push(obj)
+      }
+      setTimeout(() => {
+        this.skuLoading = false
+      },1000)
+    },
+    getCombination(array) {
+      let resultArr = [];
+      array.forEach((arrItem) => {
+        if (resultArr.length === 0) {
+          let firstItem = [];
+          arrItem.list.forEach(item => {
+            firstItem.push([item]);
+          })
+          resultArr = firstItem
+        } else {
+          console.log(0, resultArr)
+          const emptyArray = [];
+          resultArr.forEach((item) => {
+            arrItem.list.forEach((value) => {
+              emptyArray.push([...item, value])
+            })
+          })
+          resultArr = emptyArray
+        }
+      });
+      return resultArr;
     }
   }
 }
@@ -834,9 +960,59 @@ export default {
     line-height: 178px;
     text-align: center;
   }
-  .avatar {
+  .avatar-uploader >>> .avatar {
     width: 178px;
     height: 178px;
     display: block;
+  }
+  .sku-uploader >>> .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .sku-uploader >>> .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .sku-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 78px;
+    height: 78px;
+    line-height: 78px;
+    text-align: center;
+  }
+  .sku-uploader >>> .avatar {
+    width: 78px;
+    height: 78px;
+    display: block;
+  }
+  .el-tag + .el-tag {
+    margin-left: 10px;
+  }
+  .button-new-tag {
+    margin-left: 10px;
+    height: 32px;
+    line-height: 30px;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+  .input-new-tag {
+    width: 90px;
+    margin-left: 10px;
+    vertical-align: bottom;
+  }
+  .Tinymce_box { /* // 富文本样式 */
+    display: flex;
+  }
+  .editor-content {
+    margin-left: 30px;
+    flex-grow: 1;
+    border: 2px dashed #f1f1f1;
+    padding:0 20px;
+  }
+  h3 {
+    color: #808080;
   }
 </style>
